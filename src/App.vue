4ac2077206a07028f85338389b172a417feced9e
Sara <svalencoso@gmail.com>
2022-12-19 21:14:51 +0100

    Nivells 1 i 2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/HomeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HomeComponent, 
+  }, 
 }
 </script>
 
-<style>
+<style scoped lang="css">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ButtonsComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="$emit('click', -1)">Anterior</button>
+    <button @click="$emit('click', 1)">Següent</button> 
+    <!-- pasar evento y parametro -->
+</template>
+
+<script>
+export default {
+    name: "ButtonsComponent", 
+    emits: ['click'] //solo para definir el evento a emitir
+}
+</script>
+
+<style scoped lang="css">
+    button {
+        font-size: 1.7em;
+        background-color: white;
+        padding:15px;
+        margin-bottom: 15px;
+        border:solid 1px;
+        border-color:black;
+        width: 50%;
+    }
+    p {
+        text-align: center;
+    }
+</style>
